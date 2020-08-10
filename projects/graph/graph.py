d1c2582dd2628e1488302682b28491f8e90d8819
Justin Lohner <lohner4@gmail.com>
2020-08-10 15:29:02 -0600

    wrote bfs function

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -69,7 +69,15 @@ class Graph:
 
 		This should be done using recursion.
 		"""
-		pass  # TODO
+		pass
+		# visited = set()
+		# visited.add(starting_vertex)
+		# # print(starting_vertex)
+		# for n in self.get_neighbors(starting_vertex):
+		# 	if starting_vertex not in self.get_neighbors(n):
+		# 		self.dft_recursive(n)
+		# 	else:
+		# 		visited.add(n)
 
 	def bfs(self, starting_vertex, destination_vertex):
 		"""
@@ -77,7 +85,20 @@ class Graph:
 		starting_vertex to destination_vertex in
 		breath-first order.
 		"""
-		pass  # TODO
+		q = Queue()
+		q.enqueue([starting_vertex])
+		visited = set()
+		while q.size() > 0:
+			p = q.dequeue()
+			v = p[-1]
+			if v not in visited:
+				if (v == destination_vertex):
+					return p
+				visited.add(v)
+				for n in self.get_neighbors(v):
+					copy = p[:]
+					copy.append(n)
+					q.enqueue(copy)
 
 	def dfs(self, starting_vertex, destination_vertex):
 		"""
@@ -151,6 +172,9 @@ if __name__ == '__main__':
 		1, 2, 4, 6, 3, 5, 7
 	'''
 	graph.dft(1)
+
+	print('---------------')
+
 	graph.dft_recursive(1)
 
 	'''
